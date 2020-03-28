0c9c42f774a7be5cad03d68293291ad34ff3cf91
Wendy Sun <wendy.sun@tsgforce.com>
2020-03-28 15:07:53 -0400

    Updates

---
+++ precache-manifest.134af49da89d549b8bcbfa38cadc7295.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36a3f7b7997b09dbd8496061caf75f6",
+    "url": "/hows-it-hanging/index.html"
+  },
+  {
+    "revision": "ff88985ad8a1eda9159d",
+    "url": "/hows-it-hanging/static/css/main.2e8245e0.chunk.css"
+  },
+  {
+    "revision": "b545083745112f26c384",
+    "url": "/hows-it-hanging/static/js/2.337a8a55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hows-it-hanging/static/js/2.337a8a55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff88985ad8a1eda9159d",
+    "url": "/hows-it-hanging/static/js/main.6a082f3e.chunk.js"
+  },
+  {
+    "revision": "977412f783145769e6e8",
+    "url": "/hows-it-hanging/static/js/runtime-main.b24b183a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hows-it-hanging/static/media/logo.5d5d9eef.svg"
+  }
+]);
